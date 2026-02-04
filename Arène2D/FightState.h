0df58c11c16c12fec453b9e78f2239b977c798b0
eromanet <eromanet@gaming.tech>
2026-02-04 09:23:41 +0100

    ok now ghotta redo

--- Arène2D/FightState.h
+++ Arène2D/FightState.h
@@ -1,5 +1,23 @@
 #pragma once
-class FightState
-{
-};
+#include <iostream>
+#include <ostream>
 
+#include "Context.h"
+//#include "StateMachine/State.h"
+#include "State.h"
+
+namespace OrcAI
+{
+    class FightState : public FSM::State<Context>
+    {
+        void Enter(Context _context) override
+        {
+            std::cout << "Enter Roaming _State" << std::endl;
+        }
+
+        void Exit(Context _context) override
+        {
+            std::cout << "Exit Roaming _State" << std::endl;
+        }
+    };
+}
