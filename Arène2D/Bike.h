b4e3c4d5216ecd4c4d057f98bb475a993318569a
eromanet <eromanet@gaming.tech>
2026-02-05 12:30:02 +0100

    zd

--- Arène2D/Bike.h
+++ Arène2D/Bike.h
@@ -52,3 +52,28 @@ public:
 };
 
 
+
+
+
+//#include "SFML/Graphics.hpp"
+//
+//class Player
+//{
+//public:
+//
+//    Player(float startX, float startY);
+//
+//
+//    void handleInput();
+//    void update(float deltaTime);
+//    void draw(sf::RenderWindow& window);
+//
+//    sf::Vector2f getPosition() const { return m_shape.getPosition(); }
+//    sf::Vector2f getSize() const { return m_shape.getSize(); }
+//    sf::Vector2f getVelocity() const { return m_velocity; }
+//private:
+//    sf::RectangleShape m_shape;
+//    sf::Vector2f m_velocity;
+//    float m_speed;
+//};
+
